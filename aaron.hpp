a61a668c884c439f796e40a0007049bfe9ec7682
Aaron Josephs <ajoseph4@binghamton.edu>
2013-09-12 14:40:22 -0400

    fixed zip

--- aaron.hpp
+++ aaron.hpp
@@ -1,5 +1,7 @@
 #include <vector>
+#include <utility>
 #include <iterator>
+#include <algorithm>
 #include <iostream>
 namespace aaron {
     template <typename Iterator, typename Function>
@@ -13,28 +15,33 @@ namespace aaron {
             return func;
         }
     template <typename Iterator>
-        auto inc_deref (Iterator & iterator) -> decltype(*iterator){
-            Iterator temp(iterator);
+        void advance_all (Iterator & iterator) {
             ++iterator;
-            return *temp;
         }
+    template <typename Iterator, typename ... Iterators>
+        void advance_all (Iterator & iterator, Iterators& ... iterators) {
+            ++iterator;
+            advance_all(iterators...);
+        } 
     template <typename Function, typename Iterator, typename ... Iterators>
         Function zip (Function func, Iterator begin, 
                 Iterator end, 
                 Iterators ... iterators)
         {
-            for(;begin != end; ++begin)
-                func(*begin, inc_deref(iterators)... );
+            for(;begin != end; ++begin, advance_all(iterators...))
+                func(*begin, *(iterators)... );
+            //could also make this a tuple
             return func;
         }
 }
 int main () {
     std::vector<int> v1{1,2,3};
     std::vector<int> v2{3,2,1};
-    std::vector<float> v2{1.2,2.4,9.0};
+    std::vector<float> v3{1.2,2.4,9.0};
+    std::vector<float> v4{1.2,2.4,9.0};
     aaron::zip (
-            [](int i,int j,float k){
-                std::cout << i << " " << j << " " << k << std::endl;
+            [](int i,int j,float k,float l){
+                std::cout << i << " " << j << " " << k << " " << l << std::endl;
             },
-            v1.begin(),v1.end(),v2.begin());
+            v1.begin(),v1.end(),v2.begin(),v3.begin(),v4.begin());
 }
