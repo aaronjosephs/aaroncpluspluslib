85aef7e3b63ca412b0b6f86ce6cb1a0834d43711
Aaron Josephs <ajoseph4@binghamton.edu>
2013-09-12 14:56:30 -0400

    Force Commit

--- aaron.hpp
+++ aaron.hpp
@@ -33,6 +33,45 @@ namespace aaron {
             //could also make this a tuple
             return func;
         }
+    //Math functions
+    //Sieve of Eratostothenes
+    std::vector<int> generate_sieve(const ulong & max) {
+        std::vector<int> sieve{2};
+        std::vector<bool> not_prime(max+1);
+        ulong current_prime = sieve.back();
+        bool done = false;
+        while(!done){ 
+            for (int i = 2; i * current_prime <= max;i++) {
+                not_prime[i*current_prime]=true;
+            }
+            for (int j = current_prime+1;true;j++) {
+                if (not_prime[j] == false) {
+                    if( j >= max) {
+                        done = true;
+                        break;
+                    }
+                    else {
+                        sieve.push_back(j);
+                        current_prime = j;
+                        break;
+                    }
+                }
+            }
+        }
+        return sieve;
+    }
+    //Prime factorization
+    std::vector<ulong> prime_factors(ulong number) {
+        auto primes = generate_sieve(number/2+1);
+        std::vector<ulong> factors;
+        for (auto iter = primes.begin();number!=1;++iter) {
+            while(number % *iter == 0) {
+                factors.push_back(*iter);
+                number /= *iter;
+            }
+        }
+        return factors;
+    }
 }
 int main () {
     std::vector<int> v1{1,2,3};
@@ -41,7 +80,7 @@ int main () {
     std::vector<float> v4{1.2,2.4,9.0};
     aaron::zip (
             [](int i,int j,float k,float l){
-                std::cout << i << " " << j << " " << k << " " << l << std::endl;
+            std::cout << i << " " << j << " " << k << " " << l << std::endl;
             },
             v1.begin(),v1.end(),v2.begin(),v3.begin(),v4.begin());
 }
