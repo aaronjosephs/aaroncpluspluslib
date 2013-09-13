fd6aaef4a810c00f350db27bd354949c5de519c8
Aaron Josephs <ajoseph4@binghamton.edu>
2013-09-12 20:06:09 -0400

    a

--- aaron.hpp
+++ aaron.hpp
@@ -1,11 +1,13 @@
 #ifndef AARON_HPP
 #define AARON_HPP
 #include <vector>
+#include <string.h>
 #include <utility>
 #include <iterator>
 #include <algorithm>
 #include <iostream>
 namespace aaron {
+    using ulong = unsigned long long;
     template <typename Iterator, typename Function>
         Function enumerate(Iterator begin, 
                 Iterator end, 
@@ -31,7 +33,7 @@ namespace aaron {
                 Iterators ... iterators)
         {
             for(;begin != end; ++begin, advance_all(iterators...))
-                func(*begin, *(iterators)... );
+                func(*begin, (*iterators)... );
             //could also make this a tuple
             return func;
         }
@@ -74,16 +76,8 @@ namespace aaron {
         }
         return factors;
     }
-}
-int main () {
-    std::vector<int> v1{1,2,3};
-    std::vector<int> v2{3,2,1};
-    std::vector<float> v3{1.2,2.4,9.0};
-    std::vector<float> v4{1.2,2.4,9.0};
-    aaron::zip (
-            [](int i,int j,float k,float l){
-            std::cout << i << " " << j << " " << k << " " << l << std::endl;
-            },
-            v1.begin(),v1.end(),v2.begin(),v3.begin(),v4.begin());
+    int func (int i) {
+        return i;
+    }
 }
 #endif
