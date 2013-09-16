a930209faeb558b83fa0091d81ff519136ed1d9b
Aaron Josephs <ajoseph4@binghamton.edu>
2013-09-15 22:33:53 -0400

    updated test

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include "aaron.hpp"
+//#include "itertools.hpp"
 #include <vector>
 #include <array>
 
@@ -44,6 +45,12 @@ int main () {
             << aaron::zip_get<2>(i) << 
             aaron::zip_get<3>(i) << std::endl;
     }
+    for (auto i : aaron::myzip(v1,v2,v3,s6)) {
+        std::cout << aaron::zip_get<0>(i) << " " 
+            << aaron::zip_get<1>(i) << " "
+            << aaron::zip_get<2>(i) << 
+            aaron::zip_get<3>(i) << std::endl;
+    }
     std::cout << std::endl << "Chain iter test" << std::endl;
     auto range = aaron::chain(v1,v2,a5);
     for (auto iter = range.begin(); iter != range.end();++iter) {
@@ -71,7 +78,46 @@ int main () {
     //auto zippy2(zippy);
     //std::cout << (zippy2 != ++zippy) << std::endl;
     //std::cout << std::get<0>(*zippy) << " " << std::get<1>(t) << " " << std::get<2>(t) << std::endl;
+    /*
+    v1={1,2,3};
+    v2={4,5,6};
+    v3={1.2,2.4,9.0,6.7};
+    v4={1.2,2.4,9.0};
+    a5={{8,9,10}};
+    s6={"apple","pear","bannananan","fruity"};
+    std::cout << std::endl << "Reverse test" << std::endl;
+    for (auto i : iter::reverse(v1)) {
+        std::cout << i << std::endl;
+    }
+    std::cout << std::endl << "slice test" << std::endl;
+    for (auto i : iter::slice(v1,0,2)) {
+        std::cout << i << std::endl;
+    }
     
+    std::cout << std::endl << "Variadic template zip iterator" << std::endl;
+    for (auto i : iter::zip(v1,v2,v3,s6)) {
+        std::cout << iter::zip_get<0>(i) << " " 
+            << (iter::zip_get<1>(i)=22) << " "
+            << iter::zip_get<2>(i) << 
+            iter::zip_get<3>(i) << std::endl;
+    }
+    for (auto i : iter::zip(v1,v2,v3,s6)) {
+        std::cout << iter::zip_get<0>(i) << " " 
+            << iter::zip_get<1>(i) << " "
+            << iter::zip_get<2>(i) << 
+            iter::zip_get<3>(i) << std::endl;
+    }
+    std::cout << std::endl << "Chain iter test" << std::endl;
+    auto rang = iter::chain(v1,v2,a5);
+    for (auto iter = rang.begin(); iter != rang.end();++iter) {
+        std::cout << (*iter=1) << std::endl;
+    }
+    //std::cout << *(++(++(++(range.begin())))) << std::endl;
+    for (auto i : iter::chain(v1,v2,a5)) {
+        std::cout << i << std::endl;
+    } 
+    */
+  //test of same stuff in different namespace and file  
     return 0;
 }
 
